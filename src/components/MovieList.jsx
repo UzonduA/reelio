217896123d55a356ca194a820e860c48e039461b
UzonduA <agbaforuzondu@gmail.com>
2025-10-12 20:47:19 +0100

    refactor(MovieList): integrate favorites props and     simplify rendering - Added support for toggleFavorite and favorites     props. - Removed duplicate MovieCard rendering. - Ensured proper prop handling for onCardClick     and favorites toggle.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
-export default function MovieList({ movies, onCardClick }) {
+export default function MovieList({ movies, onCardClick, favorites = [], toggleFavorite = () => {} }) {
   const handleCardClick = onCardClick || (() => {});
 
   if (!movies || movies.length === 0) {
@@ -11,17 +11,13 @@ export default function MovieList({ movies, onCardClick }) {
   return (
     <div className="grid gap-6 grid-cols-1 mt-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {movies.map((movie) => (
-        <div
-          key={movie.id}
-          onClick={() => handleCardClick(movie)}
-          role="button"
-          tabIndex={0}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" || e.key === " ") handleCardClick(movie);
-          }}
-        >
-          <MovieCard movie={movie} onClick={onCardClick} />
-        </div>
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onClick={() => handleCardClick(movie)}
+            onFavorite={toggleFavorite}
+            isFavorite={favorites.some((fav) => fav.id === movie.id)}
+          />
       ))}
     </div>
   );
